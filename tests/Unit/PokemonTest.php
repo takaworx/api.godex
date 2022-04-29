f287cdfe4b90a2991a5e236792f1f666ac3a54b6
takaworx <neil@takaworx.com>
2022-04-29 15:55:10 +0800

    dislike test

--- tests/Unit/PokemonTest.php
+++ tests/Unit/PokemonTest.php
@@ -37,6 +37,30 @@ class PokemonTest extends TestCase
         ];
     }
 
+    public function dislikeTestDataProvider()
+    {
+        return [
+            'success' => [
+                'pokemon_ids' => [1],
+                'likes' => $this->generateDislikes([1]), // pass pokemon_ids here as parameter
+                'shouldRemoveLike' => false, // if user already disliked the pokemon calling the function again should remove it
+                'expected' => 'saved'
+            ],
+            'exceeded' => [
+                'pokemon_ids' => [1,2,3],
+                'likes' => $this->generateDislikes([1,2,3]), // pass pokemon_ids here as parameter
+                'shouldRemoveLike' => false, // if user already disliked the pokemon calling the function again should remove it
+                'expected' => 'exception'
+            ],
+            'remove' => [
+                'pokemon_ids' => [1],
+                'likes' => $this->generateDislikes([1]), // pass pokemon_ids here as parameter
+                'shouldRemoveLike' => true, // if user already disliked the pokemon calling the function again should remove it
+                'expected' => 'deleted'
+            ]
+        ];
+    }
+
     /**
      * @dataProvider likeTestDataProvider
      */
@@ -77,6 +101,46 @@ class PokemonTest extends TestCase
         }
     }
 
+    /**
+     * @dataProvider dislikeTestDataProvider
+     */
+    public function testDislikeFunction($pokemon_ids, $likes, $shouldRemoveLike, $expected)
+    {
+        $this->withoutExceptionHandling();
+
+        $likeMock = Mockery::mock(Like::class, function (MockInterface $mock) use ($likes) {
+            $mock->shouldReceive('newQuery')->andReturnSelf();
+            $mock->shouldReceive('where')->andReturnSelf();
+            $mock->shouldReceive('get')->andReturn($likes);
+            $mock->shouldReceive('newInstance')->andReturn($this->createLikeInstance());
+            $mock->shouldReceive('delete')->andReturn('deleted');
+        });
+
+        $service = new PokemonService($likeMock, new Dislike, new Favorite);
+
+        $user_id = 1;
+        
+        do {
+            $pokemon_id = random_int(1, 99);
+        } while(in_array($pokemon_id, $pokemon_ids));
+
+        if ($shouldRemoveLike) {
+            $pokemon_id = $pokemon_ids[0];
+        }
+
+        try {
+            $result = $service->dislike($user_id, $pokemon_id);
+
+            if ($expected === 'saved') {
+                $this->assertEquals(true, $result->saved);
+            } else if ($expected === 'deleted') {
+                $this->assertEquals($expected,  $result);
+            }
+        } catch (\Exception $e) {
+            $this->assertEquals(__('pokemon.max_dislike_exceeded'), $e->getMessage());
+        }
+    }
+
     private function generateLikes(array $ids)
     {
         $likes = [];
@@ -92,7 +156,36 @@ class PokemonTest extends TestCase
         return collect($likes);
     }
 
+    private function generateDislikes(array $ids)
+    {
+        $dislikes = [];
+
+        for ($i = 0; $i < count($ids); $i++) {
+            $dislike = $this->createDislikeInstance();
+
+            $dislike->pokemon_id = $ids[$i];
+
+            $dislikes[] = $dislike;
+        }
+
+        return collect($dislikes);
+    }
+
     private function createLikeInstance()
+    {
+        return new class {
+            public $user_id;
+            public $pokemon_id;
+            public $saved = false;
+
+            public function save()
+            {
+                return $this->saved = true;
+            }
+        };
+    }
+
+    private function createDislikeInstance()
     {
         return new class {
             public $user_id;
